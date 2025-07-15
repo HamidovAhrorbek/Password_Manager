6c360d86b41dc7a292417af21087df73a7a7eb69
Ahrorbek <123533812+HamidovAhrorbek@users.noreply.github.com>
2025-07-15 15:45:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,24 +11,29 @@ window.title("Password Manager")
 window.config(padx=20, pady=20)
 
 canvas = Canvas(width=200, height=200)
-password_img = PhotoImage(file="logo.png")
-canvas.create_image(100,100, image=password_img)
-canvas.pack()
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100,100, image=logo_img)
+canvas.grid(row=0, column=1)
 
-website_label = Label(text="Website:")
-website_label.grid(column=0, row=1, columnspan=20)
+# Labels
+website_label = Label(text="Website")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username")
+email_label.grid(row=2, column=0)
+password_label = Label(text="password")
+password_label.grid(row=3, column=0)
 
-email_label = Label(text="Email/Username:")
-email_label.grid(column=0, row=2, columnspan=20)
 
-password_label = Label(text="Password:")
-password_label.grid(column=0, row=3, columnspan=15)
+# Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
 
-generate_password = Button(text="Generate Password", highlightthickness=0)
-generate_password.grid(column=2,row=4)
 
-add = Button(text="Add", highlightthickness=0)
-add.grid(column=1,row=4)
+
 
 
 window.mainloop()
